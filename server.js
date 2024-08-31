c8937ab137c1eaf9d5c27277f646c44a86cbbb7f
aftab7604 <a_aftab.s@hotmail.com>
2024-09-01 01:32:30 +0500

    server changes

--- server.js
+++ server.js
@@ -16,17 +16,17 @@ const users = [];
 socketIO.on('connection', (socket) => {
     socket.on('connected', (userId) => {
         users[userId] = socket.id;
-        console.log(users)
+        console.log(users);
     });
 
     const connection = mysql.createConnection({
-        host: '127.0.0.1',
+        host: 'fiv2-server.mysql.database.azure.com',
         port: 3306,
-        user: 'root',
-        password: 'eDdfh15d864sR!ds',
-        database: 'test_db'
+        user: 'lqeiqcvcxf',
+        password: 'Z$DrWPfTD8TtiRKg',
+        database: 'fiv2-database'
     });
-    
+
     connection.connect((error) => {
         if (error) {
             console.error('Database connection failed: ' + error.stack);
@@ -36,33 +36,45 @@ socketIO.on('connection', (socket) => {
     });
 
     socket.on("sendLoginEventToAdmin", async function (data) {
-        console.log(data)
+        console.log(data);
         socketIO.to(users[0]).emit("userLoginNotificationReceived", 'Some info Submitted Please Refresh');
-    })
+    });
 
     socket.on("sendUserLoginPageRefreshEvent", async function (data) {
-        console.log(data)
+        console.log(data);
         socketIO.to(users[data.userId]).emit("userLoginNotificationReceived", data.type);
-    })
+    });
 
     socket.on("sendEvent", async function (data) {
-        connection.query("SELECT * FROM users WHERE id = " + data.userId, function (error, receiver) {
-            if (receiver != null) {
-                if (receiver.length > 0) {
-     
-                    connection.query("SELECT * FROM users WHERE id = " + data.myId, function (error, sender) {
-                        if (sender.length > 0) {
-                            var message = "New message received from: " + sender[0].name + ". Message: " + data.message;
-                            socketIO.to(users[receiver[0].id]).emit("messageReceived", message);
-                        }
-                    });
-                }
+        connection.query("SELECT * FROM users WHERE id = ?", [data.userId], function (error, receiver) {
+            if (error) {
+                console.error('Error fetching receiver:', error);
+                connection.end(); // Close the connection on error
+                return;
+            }
+            if (receiver != null && receiver.length > 0) {
+                connection.query("SELECT * FROM users WHERE id = ?", [data.myId], function (error, sender) {
+                    if (error) {
+                        console.error('Error fetching sender:', error);
+                    } else if (sender.length > 0) {
+                        var message = "New message received from: " + sender[0].name + ". Message: " + data.message;
+                        socketIO.to(users[receiver[0].id]).emit("messageReceived", message);
+                    }
+                    connection.end(); // Close the connection after the query is done
+                });
+            } else {
+                connection.end(); // Close the connection if no receiver found
             }
         });
     });
-    
-});
 
-http.listen(3000,() => {
-    console.log('Server is started. ');
+    socket.on('disconnect', () => {
+        console.log(`User ${socket.id} disconnected`);
+        // Optionally close the connection here if you have an open connection per socket
+        // connection.end();
+    });
+});
+const PORT = process.env.PORT || 8080;
+http.listen(PORT,() => {
+    console.log('Server is started.');
 });
